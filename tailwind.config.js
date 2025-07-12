810ff0778b3ab96b52309944c3268bd8d7258ab2
ryeong <oaoa0728@naver.com>
2025-07-12 19:00:11 +0900

    🎨 Style : tailwind 색상 커스텀 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,9 +9,28 @@ module.exports = {
         serif: ['Gowun Batang', 'serif'],
       },
       colors: {
+        // primary color
         primary: '#9BC99F',
+        primaryDark: '#6FA974',
         secondary: '#D2DEF4',
+        bgColor: '#F9F7F0',
+
+        // state color
+        error: '#F56767',
+        success: '#9BC99F', // primary랑 동일
+        disabled: '#D9D9D9',
+
+        inputBgWhite: '#FCFCFC',
         sub: '#FFF9F1',
+
+        // gray system
+        gray: {
+          900: '#2A2A2A',
+          700: '#6B6B6B',
+          500: '#A0A0A0',
+          300: '#D9D9D9',
+          200: '#F0F0F0',
+        },
       },
     },
   },
