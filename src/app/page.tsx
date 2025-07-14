5bba90ffbbd50d7d6b972784bc46197526dd2e42
ryeong <oaoa0728@naver.com>
2025-07-14 22:35:15 +0900

    ✨ Feat : 고정 푸터 마크업 및 기능 구현 #3

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import EntryAuthModal from '@/components/mainPage/entryAuthModal';
 import MainPage from '@/components/mainPage/mainPage';
 
 export default function Home() {
-  const isLogin = false;
+  const isLogin = true;
   return (
     <>
       <MainPage />
--- src/components/mainPage/mainPage.tsx
+++ src/components/mainPage/mainPage.tsx
@@ -1,3 +1,10 @@
+import FixedFooter from './fixedFooter';
+
 export default function MainPage() {
-  return <div>메인페이지</div>;
+  return (
+    <div className='pb-[177px]'>
+      <div className='w-full h-[323px] bg-[#383838]'></div>
+      <FixedFooter />
+    </div>
+  );
 }
