644a2bf973ae4572ef343c5300ad3894ba1c2de8
ryeong <oaoa0728@naver.com>
2025-07-18 15:57:53 +0900

    ✨ Feat : 고정 푸터 삭제 #7

--- src/components/mainPage/mainPage.tsx
+++ src/components/mainPage/mainPage.tsx
@@ -1,7 +1,6 @@
 import BookReport from './bookReport';
 import BookShelfSection from './bookShelfSection';
 import DeskSection from './deskSection';
-import FixedFooter from './fixedFooter';
 import Header from '../header/header';
 import HeroSection from './heroSection';
 
@@ -16,7 +15,6 @@ export default function MainPage() {
         <BookReport />
       </div>
       <div className='w-full h-[323px] bg-[#383838]'></div>
-      <FixedFooter />
     </div>
   );
 }
