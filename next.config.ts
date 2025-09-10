4f3f8ef8801618ebf7db8a2e90ec6accb7000764
ryeong <oaoa0728@naver.com>
2025-09-10 21:09:26 +0900

    ♻️ Refactor : svg 컴포넌트화 설치

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,32 @@ const nextConfig: NextConfig = {
   images: {
     domains: ['image.aladin.co.kr'],
   },
+
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: [
+        {
+          loader: '@svgr/webpack',
+          options: {
+            icon: true,
+            svgProps: { fill: 'currentColor' },
+            svgoConfig: {
+              plugins: [
+                {
+                  name: 'removeAttrs',
+                  params: { attrs: '(fill)' },
+                },
+              ],
+            },
+          },
+        },
+      ],
+    });
+
+    return config;
+  },
 };
 
 export default nextConfig;
